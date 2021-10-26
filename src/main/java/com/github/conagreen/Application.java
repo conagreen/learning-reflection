960c9a9b1133b94a19fbb4f1de56d05d17b1f206
conagreen <icoqwee@gmail.com>
2021-10-27 00:27:25 +0900

    생성자 연습

--- src/main/java/com/github/conagreen/Application.java
+++ src/main/java/com/github/conagreen/Application.java
@@ -1,42 +1,23 @@
 package com.github.conagreen;
 
 import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 
 public class Application {
 
     public static void main(String[] args) throws ClassNotFoundException {
         final Class clazz = Class.forName("com.github.conagreen.Dummy");
         System.out.println("clazz = " + clazz);
-        // java reflection api - Class
-        System.out.println(clazz.getCanonicalName());
-        System.out.println(clazz.getName());
-        System.out.println(clazz.getModifiers());
-        System.out.println("------------------------------");
-        for (Field field : clazz.getFields()) {
-            System.out.println("field = " + field);
+
+        // 생성자 API
+        for (Constructor<?> constructor : clazz.getConstructors()) {
+            System.out.println(constructor);
+            System.out.println(constructor.getDeclaringClass());
+            System.out.println(constructor.getModifiers());
+            System.out.println("-----------------------------");
+            for (Class<?> parameterType : constructor.getParameterTypes()) {
+                System.out.println("parameterType = " + parameterType);
+            }
+            System.out.println("-----------------------------");
         }
-        System.out.println("------------------------------");
-        for (Field declaredField : clazz.getDeclaredFields()) {
-            System.out.println("declaredField = " + declaredField);
-        }
-        System.out.println("------------------------------");
-        for (Constructor constructor : clazz.getConstructors()) {
-            System.out.println("constructor = " + constructor);
-        }
-        System.out.println("------------------------------");
-        for (Constructor declaredConstructor : clazz.getDeclaredConstructors()) {
-            System.out.println("declaredConstructor = " + declaredConstructor);
-        }
-        System.out.println("------------------------------");
-        for (Method method : clazz.getMethods()) {
-            System.out.println("method = " + method);
-        }
-        System.out.println("------------------------------");
-        for (Method declaredMethod : clazz.getDeclaredMethods()) {
-            System.out.println("declaredMethod = " + declaredMethod);
-        }
-        System.out.println("------------------------------");
     }
 }
