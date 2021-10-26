ed61213874e818b2e9718e08b4399f6ca456ba85
conagreen <icoqwee@gmail.com>
2021-10-27 00:46:20 +0900

    Method 클래스 메서드 연습

--- src/main/java/com/github/conagreen/Application.java
+++ src/main/java/com/github/conagreen/Application.java
@@ -1,6 +1,7 @@
 package com.github.conagreen;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class Application {
 
@@ -8,11 +9,18 @@ public class Application {
         final Class clazz = Class.forName("com.github.conagreen.Dummy");
         System.out.println("clazz = " + clazz);
 
-        final Field[] fields = clazz.getDeclaredFields();
-        for (Field field : fields) {
-            System.out.println("---------------------------");
-            System.out.println(field.getName());
-            System.out.println(field.getModifiers());
+        final Method[] methods = clazz.getMethods();
+        for (Method method : methods) {
+            System.out.println("=======================================");
+            System.out.println(method.getName());
+            final Class<?>[] parameterTypes = method.getParameterTypes();
+            for (Class<?> parameterType : parameterTypes) {
+                System.out.println("-------------------------------");
+                System.out.println("parameterType = " + parameterType);
+                System.out.println("-------------------------------");
+            }
+            System.out.println(method.getReturnType().getName());
+            System.out.println("=======================================");
         }
     }
 }
