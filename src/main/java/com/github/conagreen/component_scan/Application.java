7dcdabf540106c04584e6bf53e35cb5b720b9909
conagreen <icoqwee@gmail.com>
2021-10-29 01:12:10 +0900

    reflections 이용해서 컴포넌트 스캔 만들어보기

--- src/main/java/com/github/conagreen/component_scan/Application.java
+++ src/main/java/com/github/conagreen/component_scan/Application.java
@@ -1,7 +1,56 @@
 package com.github.conagreen.component_scan;
 
+import org.reflections.Reflections;
+import org.reflections.scanners.*;
+
+import java.util.Set;
+
 public class Application {
+
+    public Set<Class<?>> getClassWithControllerAnnotation() {
+        Reflections reflections = new Reflections(
+                "com.github.conagreen.component_scan",
+                new SubTypesScanner(),
+                new TypeAnnotationsScanner());
+        return reflections.getTypesAnnotatedWith(Controller.class);
+    }
+
+    public Set<Class<?>> getClassWithRepositoryAnnotation() {
+        Reflections reflections = new Reflections(
+                "com.github.conagreen.component_scan",
+                new SubTypesScanner(),
+                new TypeAnnotationsScanner());
+        return reflections.getTypesAnnotatedWith(Repository.class);
+    }
+
+    public Set<Class<?>> getClassWithServiceAnnotation() {
+        Reflections reflections = new Reflections(
+                "com.github.conagreen.component_scan",
+                new SubTypesScanner(),
+                new TypeAnnotationsScanner());
+        return reflections.getTypesAnnotatedWith(Service.class);
+    }
+
+
     public static void main(String[] args) {
+        Application application = new Application();
+        final Set<Class<?>> classWithControllerAnnotation = application.getClassWithControllerAnnotation();
+        System.out.println("----------------------------- @Controller -----------------------------");
+        for (Class<?> controllerClass : classWithControllerAnnotation) {
+            System.out.println("controllerClass = " + controllerClass);
+        }
+
+        System.out.println("----------------------------- @Repository -----------------------------");
+        final Set<Class<?>> classWithRepositoryAnnotation = application.getClassWithRepositoryAnnotation();
+        for (Class<?> repositoryClass : classWithRepositoryAnnotation) {
+            System.out.println("repositoryClass = " + repositoryClass);
+        }
+
+        System.out.println("----------------------------- @Service -----------------------------");
+        final Set<Class<?>> classWithServiceAnnotation = application.getClassWithServiceAnnotation();
+        for (Class<?> serviceClass : classWithServiceAnnotation) {
+            System.out.println("serviceClass = " + serviceClass);
+        }
 
     }
 }
--- src/main/java/com/github/conagreen/component_scan/Controller.java
+++ src/main/java/com/github/conagreen/component_scan/Controller.java
@@ -1,4 +1,11 @@
 package com.github.conagreen.component_scan;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface Controller {
 }
