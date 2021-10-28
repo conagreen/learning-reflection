ce1d6856b0a7a03de02e61683aa2ae8726349af3
conagreen <icoqwee@gmail.com>
2021-10-29 01:24:38 +0900

    중복되는 코드 정리

--- src/main/java/com/github/conagreen/component_scan/Application.java
+++ src/main/java/com/github/conagreen/component_scan/Application.java
@@ -1,41 +1,30 @@
 package com.github.conagreen.component_scan;
 
 import org.reflections.Reflections;
-import org.reflections.scanners.*;
 
 import java.util.Set;
 
 public class Application {
+    private final Reflections reflections = new Reflections("com.github.conagreen.component_scan");
 
     public Set<Class<?>> getClassWithControllerAnnotation() {
-        Reflections reflections = new Reflections(
-                "com.github.conagreen.component_scan",
-                new SubTypesScanner(),
-                new TypeAnnotationsScanner());
         return reflections.getTypesAnnotatedWith(Controller.class);
     }
 
     public Set<Class<?>> getClassWithRepositoryAnnotation() {
-        Reflections reflections = new Reflections(
-                "com.github.conagreen.component_scan",
-                new SubTypesScanner(),
-                new TypeAnnotationsScanner());
         return reflections.getTypesAnnotatedWith(Repository.class);
     }
 
     public Set<Class<?>> getClassWithServiceAnnotation() {
-        Reflections reflections = new Reflections(
-                "com.github.conagreen.component_scan",
-                new SubTypesScanner(),
-                new TypeAnnotationsScanner());
         return reflections.getTypesAnnotatedWith(Service.class);
     }
 
 
     public static void main(String[] args) {
         Application application = new Application();
-        final Set<Class<?>> classWithControllerAnnotation = application.getClassWithControllerAnnotation();
+
         System.out.println("----------------------------- @Controller -----------------------------");
+        final Set<Class<?>> classWithControllerAnnotation = application.getClassWithControllerAnnotation();
         for (Class<?> controllerClass : classWithControllerAnnotation) {
             System.out.println("controllerClass = " + controllerClass);
         }
